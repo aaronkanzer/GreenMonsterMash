7e2c2c0d0000ddcf0ecffc1f69495c75d8e7a450
aaronkanzer <36093535+aaronkanzer@users.noreply.github.com>
2018-09-19 18:26:12 -0400

    Update GreenMonsterAutomate.py

--- GreenMonsterAutomate.py
+++ GreenMonsterAutomate.py
@@ -1,4 +1,3 @@
-
 import datetime
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -7,7 +6,6 @@ from bs4 import BeautifulSoup
 quote_page = 'https://www.baseball-reference.com/previews/'
 
 # Call BeautifulSoup web parser
-
 
 page = urlopen(quote_page)
 soup = BeautifulSoup(page, 'html.parser')
@@ -30,6 +28,7 @@ now = datetime.datetime.now()
 
 print("\nScoreboard Set-Up for " + str(now.month) + "/" + str(now.day) + "/" + str(now.year) + "\n")
 
+
 # Define MLBGame class
 class MLBGame:
 
@@ -41,23 +40,25 @@ class MLBGame:
         self.startTime = startTime
 
     def isBostonGame(self):
-    	return (self.homeTeam == 'BOS')
+        return (self.homeTeam == 'BOS')
 
     def isNLColumn(self):
         return self.homeTeam in nlTeams
 
     def gameData(self):
-    	parseStartTime = self.startTime.split(':')
-    	return (parseStartTime[0] + parseStartTime[1] + "\n" + "#" + self.awayNumb + " " + self.awayTeam + "\n" + "#" + self.homeNumb + " " + self.homeTeam + "\n")
+        return (
+                    self.startTime + "\n" + "#" + self.awayNumb + " " + self.awayTeam + "\n" + "#" + self.homeNumb + " " + self.homeTeam + "\n")
 
     def gameTime(self):
-    	parseStartTime = self.startTime.split(':')
-    	return int(parseStartTime[0] + parseStartTime[1])
+        parseStartTime = self.startTime.split(':')
+        return int(parseStartTime[0] + parseStartTime[1])
 
 
 # Parse thru data to create MLBGame object
 
 i = 0
+
+
 while i < len(current_games):
 
     startTime = ""
@@ -98,34 +99,36 @@ while i < len(current_games):
 
     i += 1
 
-
 # Define each game
 j = 0
+
 while j < len(stringGameList) - 4:
 
-#Create each MLBGame object and place into appropriate league list
-	x = MLBGame(stringGameList[j], stringGameList[j + 1], stringGameList[j + 2], stringGameList[j + 3], stringGameList[j + 4])
-	mlbGameList.append(x)
-	if x.homeTeam not in nlTeams and len(alGames) < 7:
-		alGames.append(x)
-	else:
-		nlGames.append(x)
-	j = j + 5
+    # Create each MLBGame object and place into appropriate league list
+    x = MLBGame(stringGameList[j], stringGameList[j + 1], stringGameList[j + 2], stringGameList[j + 3],
+                stringGameList[j + 4])
+    mlbGameList.append(x)
+    if x.homeTeam not in nlTeams and len(alGames) < 7:
+        alGames.append(x)
+    else:
+        nlGames.append(x)
+    j = j + 5
 
 print("---- Games for American League Side ---- \n")
+
+
 for game in alGames:
-	if(game.homeTeam == 'BOS'):
-		redSoxGame.append(game)
-		continue
-	else:
-		print(game.gameData())
+    if (game.homeTeam == 'BOS'):
+        redSoxGame.append(game)
+        continue
+    else:
+        print(game.gameData())
 
 print("---- Games for National League Side ----\n")
-for game in nlGames:
-	print(game.gameData())
 
-print('---- Red Sox Game ----\n')
-print(redSoxGame[0].gameData())
+for game in nlGames:
+    print(game.gameData())
+
 
 
 
